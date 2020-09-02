e4a32ef8e4353a74499ed50f7e4409f74858bfaf
Eric Young <eric.young@digital.cabinet-office.gov.uk>
2020-09-02 13:54:15 +0100

    Refactor the create `.secrets` file into a function
    Better readability for future post-cookiecutter generation hooks.

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,27 +1,42 @@
 from textwrap import dedent
 
-# Get the user-defined option around whether or not they would like to create a secrets file
-require_secrets = "{{ cookiecutter.create_secrets_file }}"
 
-# Check if `require_secrets` is 'Yes'
-if require_secrets == "Yes":
+def create_secrets_file(user_option: str) -> None:
+    """Create a .secrets file depending on a user option.
 
-    # Define a header for the `.secrets file`
-    secrets_file = """
-    # Secrets go here, and can be read in by Python using `os.environ.get`:
-    #
-    #   --------------------------------------------------------
-    #   import os
-    #
-    #   EXAMPLE_SECRET = os.environ.get("EXAMPLE_SECRET")
-    #   --------------------------------------------------------
-    #
-    # This file is NOT version-controlled!
+    Args:
+        user_option (str): User option.
 
-    # Google Cloud authentication credentials
-    export GOOGLE_APPLICATION_CREDENTIALS=""
+    Returns:
+        A .secrets file created in the top-level if user_option is 'Yes', otherwise nothing.
+
     """
 
-    # Write the `.secrets` file out
-    with open(".secrets", "w") as f:
-        f.write(dedent(secrets_file).lstrip())
+    # Check if `user_option` is 'Yes'
+    if user_option == "Yes":
+
+        # Define a header for the `.secrets file`
+        secrets_file = """
+        # Secrets go here, and can be read in by Python using `os.environ.get`:
+        #
+        #   --------------------------------------------------------
+        #   import os
+        #
+        #   EXAMPLE_SECRET = os.environ.get("EXAMPLE_SECRET")
+        #   --------------------------------------------------------
+        #
+        # This file is NOT version-controlled!
+
+        # Google Cloud authentication credentials
+        export GOOGLE_APPLICATION_CREDENTIALS=""
+        """
+
+        # Write the `.secrets` file out
+        with open(".secrets", "w") as f:
+            f.write(dedent(secrets_file).lstrip())
+
+
+if __name__ == "__main__":
+
+    # Create a .secrets file, if requested
+    create_secrets_file("{{ cookiecutter.create_secrets_file }}")
