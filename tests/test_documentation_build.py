51727f6bfefa7146035c3322b09e787b81d5a309
ESKYoung <ESKYoung@users.noreply.github.com>
2021-06-17 13:09:41 +0100

    Add tests and GitHub Actions to check for broken links

---
+++ tests/test_documentation_build.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+from sphinx.cmd.build import main
+
+
+class TestDocumentation:
+    def test_build(self, tmp_path: Path) -> None:
+        """Test the build of the main `govcookiecutter` repository."""
+        assert main(["-b", "html", "docs", str(tmp_path.joinpath("_build"))]) == 0
+
+    def test_external_links(self, tmp_path: Path) -> None:
+        """Test for broken external links of the main `govcookiecutter` repository."""
+        assert (
+            main(["-b", "linkcheck", "docs", str(tmp_path.joinpath("_linkcheck"))]) == 0
+        )
