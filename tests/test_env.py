150fc53a01a211acde70442eb3a38eb32bfce4bd
ESKYoung <eric.sk.young@gmail.com>
2021-06-21 10:21:41 +0100

    Update tests due to changes to `.envrc` and `.env`
    Also added `python-dotenv` as a requirement to enable this
    testing, and the use of `.env` and `.secrets` going forward.

--- tests/test_env.py
+++ tests/test_env.py
@@ -1,9 +1,10 @@
+from dotenv import dotenv_values
 from pathlib import Path
 from typing import Dict, List
 
-# Define a path to the `govcookiecutter` template directory, and its `.envrc` file
+# Define a path to the `govcookiecutter` template directory, and its `.env` file
 DIR_TEMPLATE = Path("{{ cookiecutter.repo_name }}")
-PATH_TEMPLATE_ENVRC = DIR_TEMPLATE.joinpath(".envrc")
+PATH_TEMPLATE_ENV = DIR_TEMPLATE.joinpath(".env")
 
 # Define a list of directory names to recursively ignore, as well as a list of
 # directory names to ignore at the root-level of the `govcookiecutter` template
@@ -13,45 +14,34 @@ EXCLUDE_ROOT_DIR_NAMES = [*EXCLUDE_DIR_NAMES, ".govcookiecutter"]
 EXCLUDE_SUB_DIR_IN_PARENTS_NAMES = [*EXCLUDE_ROOT_DIR_NAMES, "docs"]
 
 
-def get_actual_envrc_variables(path_envrc: Path) -> Dict[str, Path]:
-    """Get the export variables and values for directories in the `.envrc` file of the
-    `govcookiecutter` template.
+def get_actual_env_variables(path_env: Path) -> Dict[str, Path]:
+    """Get the environment variables and values for directories in the `.env` file of
+     the `govcookiecutter` template.
 
-    Args:
-        path_envrc: A file path to the `.envrc` file of the `govcookiecutter` template.
+    Args:v
+        path_env: A file path to the `.env` file of the `govcookiecutter` template.
 
     Returns:
-        A dictionary where the keys are the names of the export directory variables,
-        and the values are the export directory values.
+        A dictionary where the keys are the names of the directory variables, and the
+        values are the directory paths.
 
     """
-
-    # Instantiate a saving dictionary variable
-    envrc_actual_dir_variable = {}
-
-    # Open the `path_envrc` file, and get all export variables starting with `DIR`, and
-    # the values assigned to them, and return the output
-    with path_envrc.open() as f:
-        for line in f.readlines():
-            if line.startswith("export DIR"):
-                k, v = (
-                    line.lstrip("export ")
-                    .strip()
-                    .replace("$(pwd)", path_envrc.parent.name)
-                    .split("=")
-                )
-                envrc_actual_dir_variable[k] = Path(v)
-    return envrc_actual_dir_variable
+    directory_variables = {}
+    for variable, value in dotenv_values(path_env).items():
+        if variable.startswith("DIR_"):
+            path_value = path_env.parent.joinpath(value).resolve()
+            directory_variables[variable] = path_value.relative_to(Path.cwd())
+    return directory_variables
 
 
-def define_expected_envrc_variables(
+def define_expected_env_variables(
     folder: Path,
     exclude_folders: List[str],
     exclude_root_folders: List[str],
     exclude_sub_folders_in_parent_folders: List[str],
 ) -> Dict[str, Path]:
-    """Get the expected export directory variables and values in the `.envrc` file of
-    the `govcookiecutter` template.
+    """Get the expected directory variables and values in the `.env` file of the
+    `govcookiecutter` template.
 
     Args:
         folder: A folder path to the `govcookiecutter` template folder.
@@ -61,16 +51,16 @@ def define_expected_envrc_variables(
             of `folder` where their sub-folders should be ignored.
 
     Returns:
-        A dictionary where the keys are expected export directory variables, and the
-        values are the expected export directory values of the `.envrc` file in the
-        `govcookiecutter` template directory `folder`.
+        A dictionary where the keys are expected directory variables, and the values
+        are the expected directory paths of the `.env` file in the `govcookiecutter`
+        template directory `folder`.
 
     """
 
     # Get the names of all root-level directories in `folder`, where each name is in
     # upper case in the format "DIR_<<<DIRECTORY_NAME>>>". Ignore any directories
     # with a name in `exclude_root_folders`
-    envrc_expected_dir_variable = {
+    env_expected_dir_variable = {
         f"DIR_{d.name.upper()}": d
         for d in folder.glob("*")
         if d.is_dir() and d.name not in exclude_root_folders
@@ -80,35 +70,34 @@ def define_expected_envrc_variables(
     # name is not in `exclude_folders`, and the parent directory name is not in
     # `exclude_sub_folders_in_parent_folders`. Each name is in upper case in the
     # format "DIR_<<<PARENT_DIRECTORY_NAME>>>_<<<DIRECTORY_NAME>>>". Return
-    # `envrc_expected_dir_variable`
+    # `env_expected_dir_variable`
     for d in folder.glob("*/*"):
         if (
             d.is_dir()
             and d.name not in exclude_folders
             and d.parent.name not in exclude_sub_folders_in_parent_folders
         ):
-            envrc_expected_dir_variable[
+            env_expected_dir_variable[
                 f"DIR_{d.parent.name.upper()}_{d.name.upper()}"
             ] = d
-    return envrc_expected_dir_variable
+    return env_expected_dir_variable
 
 
-class TestEnvrcExportDirectories:
+class TestEnvExportDirectories:
     def test_values_are_directories(self) -> None:
-        """Test that the export values are real dictionaries."""
-        for n, d in get_actual_envrc_variables(PATH_TEMPLATE_ENVRC).items():
+        """Test that the values are real directories."""
+        for n, d in get_actual_env_variables(PATH_TEMPLATE_ENV).items():
             assert d.is_dir(), f"`{n}` variable directory does not exist: {d}"
 
     def test_variables_as_expected(self) -> None:
-        """Test that the export directory variable and value names in `.envrc` are as
-        expected."""
+        """Test directory variable and value names in `.env` are as expected."""
 
-        # Get the expected `.envrc` directory variables, and assert the actual
+        # Get the expected `.env` directory variables, and assert the actual
         # variables are as expected
-        test_expected = define_expected_envrc_variables(
+        test_expected = define_expected_env_variables(
             DIR_TEMPLATE,
             EXCLUDE_DIR_NAMES,
             EXCLUDE_ROOT_DIR_NAMES,
             EXCLUDE_SUB_DIR_IN_PARENTS_NAMES,
         )
-        assert get_actual_envrc_variables(PATH_TEMPLATE_ENVRC) == test_expected
+        assert get_actual_env_variables(PATH_TEMPLATE_ENV) == test_expected
