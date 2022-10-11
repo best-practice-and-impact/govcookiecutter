f60d48797ffac7b65ef3cf86e53232ff641ea5e1
colej <jacob.cole@ons.gov.uk>
2022-10-11 12:10:59 +0100

    Update help and default in make.bat

--- {{ cookiecutter.repo_name }}/make.bat
+++ {{ cookiecutter.repo_name }}/make.bat
@@ -1,7 +1,7 @@
 @echo off
 
 
-IF /I "%1"==".DEFAULT_GOAL " GOTO .DEFAULT_GOAL
+IF /I "%1"=="" GOTO .DEFAULT_GOAL
 IF /I "%1"=="requirements" GOTO requirements
 IF /I "%1"=="prepare_docs_folder" GOTO prepare_docs_folder
 IF /I "%1"=="docs" GOTO docs
@@ -13,9 +13,7 @@ IF /I "%1"=="help" GOTO help
 GOTO error
 
 :.DEFAULT_GOAL
-	CALL make.bat =
-	CALL make.bat help
-	GOTO :EOF
+	GOTO help
 
 :requirements
 	python -m pip install -U pip setuptools
@@ -56,12 +54,7 @@ GOTO error
 	GOTO :EOF
 
 :help
-	@echo "$%tput bold%Available rules:$%tput sgr0%"
-	@echo
-	@sed -n -e "/^
-	h; s/.*//; :doc" -e "H; n; s/^
-	t doc" -e "s/:.*//; G; s/\\n
-	s/\\n/ /g; p; }" %MAKEFILE_LIST% | LC_ALL='C' sort --ignore-case | awk -F '---' -v ncol=$%tput cols% -v indent=25 -v col_on="$%tput setaf 6%" -v col_off="$%tput sgr0%" '{ printf "%s%*s%s ", col_on, -indent, $$1, col_off; n = split($$2, words, " "); line_length = ncol - indent; for (i = 1; i <= n; i++) { line_length -= length(words[i]) + 1; if (line_length <= 0) { line_length = ncol - indent - length(words[i]) - 1; printf "\n%*s ", -indent, " "; } printf "%s ", words[i]; } printf "\n"; }' | more %shell test $(shell uname% = Darwin && echo '--no-init --raw-control-chars')
+	ECHO make: Use one of the following commands: requirements, docs, docs_check_external_links, coverage, coverage_html, coverage_xml.
 	GOTO :EOF
 
 :error
