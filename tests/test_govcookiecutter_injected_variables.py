91c94546beca22cde6e74e9a3f6f75753c9f4eb9
Lynch <Joshua.Lynch@ext.ons.gov.uk>
2023-01-04 14:38:11 +0000

    fix: update injected variable count

--- tests/test_govcookiecutter_injected_variables.py
+++ tests/test_govcookiecutter_injected_variables.py
@@ -165,7 +165,7 @@ args_injected_counts_correct = [
     (
         "repo_name",
         "repo_2",
-        {**REPO_NAME_COUNT, "{{ cookiecutter.repo_name }}": 3},
+        {**REPO_NAME_COUNT, "{{ cookiecutter.repo_name }}": 11},
         {"using_R": "Yes"},
     ),
     ("overview", "overview_1", OVERVIEW_COUNT, {"using_R": "No"}),
