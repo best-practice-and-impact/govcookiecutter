6e15d8552cf857c05549c0f8fd5afce6378c167d
Lynch <Joshua.Lynch@ext.ons.gov.uk>
2023-02-22 15:09:32 +0000

    add: add demo files for pytest

---
+++ {{ cookiecutter.repo_name }}/src/{{ cookiecutter.repo_name }}/utils/example_module.py
@@ -0,0 +1,6 @@
+def hello_world(name):
+    if isinstance(string, str):
+        string = "hello world and hello " + name
+        return string
+    else:
+        raise TypeError("The 'name' variable entered was not a string")
