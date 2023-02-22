39b19dcf662b2561f70017f9df56da079160ade1
Lynch <Joshua.Lynch@ext.ons.gov.uk>
2023-02-22 14:35:44 +0000

    fix: update repo injected variables

--- tests/test_govcookiecutter_injected_variables.py
+++ tests/test_govcookiecutter_injected_variables.py
@@ -34,7 +34,7 @@ REPO_NAME_COUNT = {
     "`{{ cookiecutter.repo_name }}`,": 1,
     '"{{ cookiecutter.repo_name }}.tex",': 0,
     '"{{ cookiecutter.repo_name }}doc"': 1,
-    "{{ cookiecutter.repo_name }}": 4,
+    "{{ cookiecutter.repo_name }}": 3,
 }
 OVERVIEW_COUNT = {
     '"{{ cookiecutter.overview }}",': 0,
@@ -165,7 +165,7 @@ args_injected_counts_correct = [
     (
         "repo_name",
         "repo_2",
-        {**REPO_NAME_COUNT, "{{ cookiecutter.repo_name }}": 5},
+        {**REPO_NAME_COUNT, "{{ cookiecutter.repo_name }}": 4},
         {"using_R": "Yes"},
     ),
     ("overview", "overview_1", OVERVIEW_COUNT, {"using_R": "No"}),
