39fe72a4fca7d0a88d030cfadb12871024380b13
Lynch <Joshua.Lynch@ext.ons.gov.uk>
2023-03-09 15:58:40 +0000

    fix: update pipeline to run through console using main

--- {{ cookiecutter.repo_name }}/src/{{ cookiecutter.repo_name.lower().replace(' ', '_').replace('-', '_') }}/run_pipeline.py
+++ {{ cookiecutter.repo_name }}/src/{{ cookiecutter.repo_name.lower().replace(' ', '_').replace('-', '_') }}/run_pipeline.py
@@ -5,14 +5,19 @@ from {{ cookiecutter.repo_name.lower().replace(' ', '_').replace('-', '_') }}.ex
 )
 import yaml
 
-with open("example_config.yml", "r") as file:
-    example_config = yaml.safe_load(file)
+
+def main():
+    with open("example_config.yml", "r") as file:
+        example_config = yaml.safe_load(file)
+
+    name = example_config["user_name"]
+    company = example_config["company"]
+    favourite_number = example_config["favourite_number"]
+
+    hello_string = hello_world(name, company)
+    print_string(hello_string)
+    print_favourite_number(favourite_number, name)
 
 
-name = example_config["user_name"]
-company = example_config["company"]
-favourite_number = example_config["favourite_number"]
-
-hello_string = hello_world(name, company)
-print_string(hello_string)
-print_favourite_number(favourite_number, name)
+if __name__ == "__main__":
+    main()
