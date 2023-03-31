a84f969cc59f0f0c387e6535f062cd6cbd9b70a0
Lynch <Joshua.Lynch@ext.ons.gov.uk>
2023-03-31 14:25:14 +0100

    fix: update init.py style to match isort

--- hooks/__init__.py
+++ hooks/__init__.py
@@ -1,7 +1,4 @@
-from hooks.post_gen_project import (
-    delete_files_and_folders,
-    parse_features_json,
-)
+from hooks.post_gen_project import delete_files_and_folders, parse_features_json
 from hooks.pre_gen_project import check_valid_email_address_format
 
 __all__ = (
