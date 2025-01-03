2a4d9cf658607cd6397ed537caf2344ccf09352c
Jordan Day <jordan.day@ons.gov.uk>
2025-01-03 16:12:58 +0000

    removing govuk theme settings

--- docs/conf.py
+++ docs/conf.py
@@ -74,7 +74,7 @@ html_context = {
 
 # Theme options are theme-specific and customize the look and feel of a theme further.
 # For a list of options available for each theme, see the documentation.
-html_theme_options = {"organisation": "ONS", "phase": "Alpha"}
+# html_theme_options = {"organisation": "ONS", "phase": "Alpha"}
 
 # Add any paths that contain custom static files (such as style sheets) here, relative
 # to this directory. They are copied after the builtin static files, so a file named
