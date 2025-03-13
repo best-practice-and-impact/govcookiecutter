b2804136d9f48e3e6a86f70e4b26f972f19520eb
Rosetta <135234996+gisellerosetta@users.noreply.github.com>
2025-03-13 11:27:55 +0000

    update function import in tests

--- {{ cookiecutter.repo_name }}/tests/test_example_module.py
+++ {{ cookiecutter.repo_name }}/tests/test_example_module.py
@@ -1,6 +1,6 @@
 import pytest
 
-from {{ cookiecutter.repo_name }}.{{ cookiecutter.repo_name.lower().replace(' ', '_').replace('-', '_') }}.example_modules.example_module import (
+from some_folder.example_modules.example_module import (
     hello_world,
     print_favourite_number,
     print_string,
