d7359aae01ae03dbdfe862e3cde9d02151515e32
Jordan Day <jordan.day@ons.gov.uk>
2025-03-17 09:23:28 +0000

    Temp bypass line length check

--- tests/test_env.py
+++ tests/test_env.py
@@ -117,7 +117,10 @@ def define_expected_env_variables(
             and d.name not in exclude_folders
             and d.parent.name not in exclude_sub_folders_in_parent_folders
         ):
-            if d.name.upper() == "{{ cookiecutter.repo_name.lower().replace(' ', '_').replace('-', '_') }}".upper():
+            if (
+                d.name.upper()
+                == "{{ cookiecutter.repo_name.lower().replace(' ', '_').replace('-', '_') }}".upper()  # noqa: E501
+            ):
                 env_expected_dir_variable = loop_directories_children(
                     d, env_expected_dir_variable
                 )
