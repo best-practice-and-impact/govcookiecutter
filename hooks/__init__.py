75eed8f4a23d64f042725fd01d102f8c2f410d5f
Jordan Day <jordan.day@ons.gov.uk>
2025-03-21 11:33:29 +0000

    remove imports from check email function

--- hooks/__init__.py
+++ hooks/__init__.py
@@ -5,8 +5,6 @@ from hooks.post_gen_project import (
     set_request_template,
 )
 
-# from hooks.pre_gen_project import check_valid_email_address_format
-
 __all__ = (
     "delete_files_and_folders",
     "parse_features_json",
