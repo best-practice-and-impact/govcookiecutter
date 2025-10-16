fdd4558e4e0fe60a413450b15d3d10d733549089
jday7879 <57715292+Jday7879@users.noreply.github.com>
2025-10-16 15:19:23 +0100

    Updating unit test and removing un-used file

--- tests/test_env.py
+++ tests/test_env.py
@@ -11,8 +11,8 @@ PATH_TEMPLATE_ENV = DIR_TEMPLATE.joinpath(".env")
 # directory names to ignore at the root-level of the `govcookiecutter` template
 # directory, and a list of dictionary names to ignore in certain root-level directories
 EXCLUDE_DIR_NAMES = ["__pycache__", "example_modules"]
-EXCLUDE_ROOT_DIR_NAMES = [*EXCLUDE_DIR_NAMES, ".govcookiecutter"]
-EXCLUDE_SUB_DIR_IN_PARENTS_NAMES = [*EXCLUDE_ROOT_DIR_NAMES, "docs"]
+EXCLUDE_ROOT_DIR_NAMES = [*EXCLUDE_DIR_NAMES, ".govcookiecutter", "data"]
+EXCLUDE_SUB_DIR_IN_PARENTS_NAMES = [*EXCLUDE_ROOT_DIR_NAMES, "docs", "data"]
 
 
 def remove_brackets_and_spaces(path_name: str) -> str:
